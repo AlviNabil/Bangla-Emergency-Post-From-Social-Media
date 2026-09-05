363b1e34f02206d8b010fa4cfd3b5090877ce8e3
billMac <alvi.nabil.aan@gmail.com>
2026-09-05 10:37:00 +0600

    Refactor configuration files for consistency and clarity in token definitions

--- ModelTraining/models/config.py
+++ ModelTraining/models/config.py
@@ -2,59 +2,27 @@ from transformers import AutoTokenizer, AutoModel, XLMRobertaModel, XLMRobertaTo
 
 # special tokens indices in different models available in transformers
 TOKEN_IDX = {
-    'bert': {
-        'START_SEQ': 101,
-        'PAD': 0,
-        'END_SEQ': 102,
-        'UNK': 100
-    },
-    'xlm': {
-        'START_SEQ': 0,
-        'PAD': 2,
-        'END_SEQ': 1,
-        'UNK': 3
-    },
-    'roberta': {
-        'START_SEQ': 0,
-        'PAD': 1,
-        'END_SEQ': 2,
-        'UNK': 3
-    },
-    'albert': {
-        'START_SEQ': 2,
-        'PAD': 0,
-        'END_SEQ': 3,
-        'UNK': 1
-    },
+    "bert": {"START_SEQ": 101, "PAD": 0, "END_SEQ": 102, "UNK": 100},
+    "xlm": {"START_SEQ": 0, "PAD": 2, "END_SEQ": 1, "UNK": 3},
+    "roberta": {"START_SEQ": 0, "PAD": 1, "END_SEQ": 2, "UNK": 3},
+    "albert": {"START_SEQ": 2, "PAD": 0, "END_SEQ": 3, "UNK": 1},
 }
 
 
 TOKENS = {
-    'bert': {
-        'START_SEQ': '[CLS]',
-        'PAD': '[PAD]',
-        'END_SEQ': '[SEP]',
-        'UNK': '[UNK]'
-    },
-    'roberta': {
-        'START_SEQ': '<s>',
-        'PAD': '<pad>',
-        'END_SEQ': '</s>',
-        'UNK': '<unk>'
-    },
+    "bert": {"START_SEQ": "[CLS]", "PAD": "[PAD]", "END_SEQ": "[SEP]", "UNK": "[UNK]"},
+    "roberta": {"START_SEQ": "<s>", "PAD": "<pad>", "END_SEQ": "</s>", "UNK": "<unk>"},
 }
 
 # pretrained model name: (model class, model tokenizer, output dimension, token style)
 MODELS = {
-    'bert-base-multilingual-cased': (AutoModel, AutoTokenizer, 768, 'bert'),
-    'bert-base-multilingual-uncased': (AutoModel, AutoTokenizer, 768, 'bert'),
-    'xlm-mlm-en-2048': (XLMRobertaModel, XLMRobertaTokenizer, 2048, 'xlm'),
-    'xlm-mlm-100-1280': (XLMRobertaModel, XLMRobertaTokenizer, 1280, 'xlm'),
-    'distilbert-base-multilingual-cased': (AutoModel, AutoTokenizer, 768, 'bert'),
-    'xlm-roberta-base': (XLMRobertaModel, XLMRobertaTokenizer, 768, 'roberta'),
-    'xlm-roberta-large': (XLMRobertaModel, XLMRobertaTokenizer, 1024, 'roberta'),
-    'monsoon-nlp/bangla-electra': (AutoModel, AutoTokenizer, 256, 'bert'),
-    'sagorsarker/bangla-bert-base': (AutoModel, AutoTokenizer, 768, 'bert'),
-    'xlm-roberta-base': (XLMRobertaModel, XLMRobertaTokenizer, 768, 'roberta')
-
+    "bert-base-multilingual-cased": (AutoModel, AutoTokenizer, 768, "bert"),
+    "bert-base-multilingual-uncased": (AutoModel, AutoTokenizer, 768, "bert"),
+    "xlm-mlm-en-2048": (XLMRobertaModel, XLMRobertaTokenizer, 2048, "xlm"),
+    "xlm-mlm-100-1280": (XLMRobertaModel, XLMRobertaTokenizer, 1280, "xlm"),
+    "distilbert-base-multilingual-cased": (AutoModel, AutoTokenizer, 768, "bert"),
+    "xlm-roberta-base": (XLMRobertaModel, XLMRobertaTokenizer, 768, "roberta"),
+    "xlm-roberta-large": (XLMRobertaModel, XLMRobertaTokenizer, 1024, "roberta"),
+    "monsoon-nlp/bangla-electra": (AutoModel, AutoTokenizer, 256, "bert"),
+    "sagorsarker/bangla-bert-base": (AutoModel, AutoTokenizer, 768, "bert"),
 }
