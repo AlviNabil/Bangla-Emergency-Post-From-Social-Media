42ded99902be2a9f9dac08b5a0de3f974e10ef72
billMac <alvi.nabil.aan@gmail.com>
2026-09-05 10:35:37 +0600

    Fix model name in training script for consistency

--- ModelTraining/scripts/train.sh
+++ ModelTraining/scripts/train.sh
@@ -2,5 +2,5 @@
 python train.py --model=xlm-roberta-base --task=social-media --batch-size=16 --lr=0.00001 --save-path='logs/social-media/xlm-base'
 
 # other transformer backbones reported in the paper
-# python train.py --model=bert-base-multilingual-cased --task=social-media --batch-size=16 --lr=0.00003 --save-path='logs/social-media/mbert'
+# python train.py --model=bert-base-multilingual-uncased --task=social-media --batch-size=16 --lr=0.00003 --save-path='logs/social-media/mbert'
 # python train.py --model=sagorsarker/bangla-bert-base --task=social-media --batch-size=16 --lr=0.00003 --save-path='logs/social-media/banglabert'
